7c6908d4368e4fadbdcfc4e9f1ca3e9cd5ef3034
Tony Xu <yihan.xu@gmail.com>
2018-08-04 15:31:48 -0700

    Updated view event and add hit counter

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,64 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const axios      = require('axios')
-const dotenv     = require('dotenv')
-const config = dotenv.config().parsed
-var useragent = require('useragent');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
+const config = dotenv.config().parsed;
+var useragent = require("useragent");
 
 const app = express();
 
-app.get('/viewEvent', (req, res) => {
-    var ip = req.query.ip;
-    axios.get(`https://api.ipdata.co/${ip}?api-key=${config.ip_key}`)
-    .then((response) => {
-        var agent = useragent.parse(req.headers['user-agent']);
-        var ipInfo = response.data
-        var target = req.query.page || 'My Blog'
-        var responseText = `*Visit: ${target}* \nIP: [${ip}](https://ipinfo.io/${ip}) \nCity: ${ipInfo.city} \nCountry: ${ipInfo.emoji_flag}${ipInfo.country_name} \nOrganization: ${ipInfo.organisation} \nAgent: ${agent.toString()}`
-        var requestUrl = `${config.telegramWebHookURI}/sendMessage?chat_id=507960755&parse_mode=Markdown&disable_web_page_preview=true&text=${responseText}`
-        return axios.get(encodeURI(requestUrl))
-        .then((response) => {
-            return res.json(response.data)
+var hitCounter = function(req, res, next) {
+  console.log("HIT COUNTER");
+  const counterRef = admin.database().ref("/hit_counter");
+  counterRef
+    .transaction(current => {
+      return (current || 0) + 1;
+    })
+  next();
+};
+
+app.use(hitCounter);
+
+app.get("/viewEvent", (req, res) => {
+  console.log("VIEW EVENT");
+  var ip = req.query.ip;
+  if (!req.query.ip) {
+    return res.send("NO IP");
+  }
+  axios
+    .get(`https://api.ipdata.co/${ip}?api-key=${config.ip_key}`)
+    .then(response => {
+      var agent = useragent.parse(req.headers["user-agent"]);
+      var ipInfo = response.data;
+      var target = req.query.page || "My Blog";
+      var referrer = req.query.referrer || "None";
+      var responseText = `*Visit: ${target}* \nIP: [${ip}](https://ipinfo.io/${ip}) \nReferrer: ${referrer} \nCity: ${
+        ipInfo.city
+      } \nCountry: ${ipInfo.emoji_flag}${ipInfo.country_name} \nOrganization: ${
+        ipInfo.organisation
+      } \nAgent: ${agent.toString()}`;
+      var requestUrl = `${
+        config.telegramWebHookURI
+      }/sendMessage?chat_id=507960755&parse_mode=Markdown&disable_web_page_preview=true&text=${responseText}`;
+      return axios
+        .get(encodeURI(requestUrl))
+        .then(response => {
+          return res.json(response.data);
         })
-        .catch((error) => {
-            return res.send(error)
+        .catch(error => {
+          return res.send(error);
         });
     })
-    .catch((error) => {
-        console.log(error)
-        return res.send(error)
+    .catch(error => {
+      console.log(error);
+      return res.send(error);
     });
 });
 
 const main = express();
-main.use('/api', app);
+main.use("/api", app);
 
 exports.api = functions.https.onRequest(main);
