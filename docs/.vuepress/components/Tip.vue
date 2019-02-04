cc20e235a22655fb2872025bd988d32c61269ddb
Tony Xu <yihan.xu@gmail.com>
2019-02-03 21:21:00 -0800

    Fix SSR issue

--- docs/.vuepress/components/Tip.vue
+++ docs/.vuepress/components/Tip.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="sticky-side-bar">
-    <a v-bind:href="tipUrl"><font-awesome-icon icon="dollar-sign" /></a>
-    <a v-bind:href="faceboolUrl" target="_blank"><font-awesome-icon :icon="['fab', 'facebook-f']" /></a>
-    <a v-bind:href="linkedinUrl"><font-awesome-icon prefix="fab" :icon="['fab', 'linkedin-in']" /></a>
+    <a v-bind:href="tipUrl">
+      <font-awesome-icon icon="dollar-sign"/>
+    </a>
+    <a
+      v-bind:href="'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url)"
+      target="_blank"
+    >
+      <font-awesome-icon :icon="['fab', 'facebook-f']"/>
+    </a>
+    <a
+      v-bind:href="'https://www.linkedin.com/shareArticle?mini=true&source=' + encodeURIComponent(url)"
+    >
+      <font-awesome-icon prefix="fab" :icon="['fab', 'linkedin-in']"/>
+    </a>
   </div>
 </template>
 
 <style lang='stylus' scoped>
 .sticky-side-bar {
   position: fixed;
-  z-index:1;
+  z-index: 1;
   top: 50%;
   right: 0rem;
   -webkit-transform: translateY(-50%);
@@ -34,17 +45,20 @@
 </style>
 
 <script>
+
+let URI = window.location.href || "";
+
 export default {
   name: "Tip",
+  props: {
+    url: {
+      type: String,
+      default: URI
+    }
+  },
   computed: {
-    faceboolUrl() {
-      return `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`
-    },
-    linkedinUrl() {
-      return `https://www.linkedin.com/shareArticle?mini=true&source=${encodeURIComponent(window.location.href)}`
-    },
     tipUrl() {
-      return this.$lang === 'en-US' ? '/tip/' : '/zh/tip/'
+      return this.$lang === "en-US" ? "/tip/" : "/zh/tip/";
     }
   }
 };
